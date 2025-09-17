669422dee74846f737a3ffc3a74972f6cf777c0a
micko112 <dimitrijemitic112@gmail.com>
2025-09-17 17:45:19 +0200

    prvi komit, postavljanje front i back foldera

---
+++ rentaCarBackend/src/main/java/com/rentaCar/rentaCarBackend/RentaCarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rentaCar.rentaCarBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentaCarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentaCarBackendApplication.class, args);
+	}
+
+}
---
+++ rentaCarBackend/src/test/java/com/rentaCar/rentaCarBackend/RentaCarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentaCar.rentaCarBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentaCarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
