15070acae9b7bc92af624a78dcf1efa532d2dcfe
micko112 <dimitrijemitic112@gmail.com>
2025-09-20 22:30:35 +0200

    dodat je ts, css i html za pocetnu stranicu, i promenjen globalni css

--- rentaCarFrontend/src/app/app.component.ts
+++ rentaCarFrontend/src/app/app.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from './services/user.service';
+import { ClientService } from './services/client.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: false
 })
 export class AppComponent {
-  title = 'Rent a Car';
+  title = 'rent-a-car-frontend';
+
+
+  constructor(
+    public userService: UserService,
+    public clientService: ClientService,
+    private router: Router
+  ) {}
+
+
+  logout() {
+    if (this.userService.getUserStatus() === 'admin') {
+      this.userService.logOut();
+    }
+    if (this.clientService.getUserStatus() === 'user') {
+      this.clientService.logOut();
+    }
+    this.router.navigate(['/login']);
+  }
 }
